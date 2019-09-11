35a5c17169321bccb6dd4fcdd20141253dee0b9d
Rafael Freitas da Silva <RafaelFreitasSilva@users.noreply.github.com>
2019-09-10 22:45:38 -0300

    Restruturando repositórios particular.
    Pasta Alura e Nanodegree-Udacity-Web-Front-End alteradas de diretório.

---
+++ Alura/Curso Vue.js I/src/routes.js
@@ -0,0 +1,11 @@
+import Home from './components/home/Home.vue';
+import Cadastro from './components/cadastro/Cadastro.vue';
+
+// adicionando a propriedade título
+
+export const routes = [
+
+    { path: '', component: Home, titulo: 'Home' },
+    { path: '/cadastro', component: Cadastro, titulo: 'Cadastro' }
+
+];
---
+++ Alura/README.md
@@ -0,0 +1,3 @@
+Projetos realizados como estudo para desenvolvimento Web com HTML, CSS e JavaScript
+
+link do Site: https://www.alura.com.br/
