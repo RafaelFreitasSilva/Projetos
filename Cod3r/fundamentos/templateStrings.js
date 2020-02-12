fd8013d3bd5ebd8f38316815c1965bba6df2294c
Rafael Freitas da Silva <RafaelFreitasSilva@users.noreply.github.com>
2020-02-12 12:44:46 -0300

    Fundamentos Módulo 2

---
+++ Cod3r/fundamentos/templateStrings.js
@@ -0,0 +1,11 @@
+const nome = "Rebeca";
+const concatenacao = "Olá " + nome + "!";
+const template = `
+    olá
+${ nome } !`;
+
+//Expressoes
+
+console.log(`1 + 1 = ${1 +1}`);
+const up = texto => texto.toUpperCase();
+console.log(`Ei... ${up('cuidado')}!)`)
